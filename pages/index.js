669b56d9555a5069b09106cbb7bb068fb848768b
vinicius <viniciusg.borges@outlook.com>
2025-12-17 00:36:03 -0300

    feat: change page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,27 @@
 function Home() {
-    return <h1>Te amo moguinha kubicheki</h1>
+    return (
+        <div>
+            <h1>Te amo moguinha kubicheki</h1>
+
+            <img src="/images/meu_amor.jpeg" alt="Nossa família" />
+
+            <div>
+                <h1>Amo o pão</h1>
+                {/* Espaço para inserir imagem */}
+                <img src="/images/pao.jpeg" alt="Pão" />
+            </div>
+
+            <div>
+                <h1>Amo o Pitico</h1>
+                {/* Espaço para inserir imagem */}
+                <img src="/images/pitico1.jpeg" alt="Pitico" />
+            </div>
+
+            <div>
+                <h1>Amo nossa familia ❤️</h1>
+            </div>
+        </div>
+    )
 }
 
 
